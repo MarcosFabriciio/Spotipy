1341204203199ceef7efd161894f2116b2beda3a
Marcos Fabricio <mfos@ic.ufal.br>
2019-03-26 23:28:27 -0300

    Some minor changes, see next day

--- Spotify.py
+++ Spotify.py
@@ -13,17 +13,18 @@ client_id= "c3bbdacb4cbd494bb06a9f1a0f8000e3"
 client_secret= "8ddbddaf415b437a91367a53132c870f"
 redirect_uri='https://www.google.com/'
 username='Marcos Fabricio'
-playlist = '6vl3AzV6Eh7jd23nzKCM47'
+playlist = '5owNACLGzpNxYbmh1TCBYS'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 scope = 'user-library-read playlist-read-private'
 
-# try:
-#     token = util.prompt_for_user_token(username, scope,client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
-#     sp=spotipy.Spotify(auth= token)
+try:
+    token = util.prompt_for_user_token(username, scope,client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    sp=spotipy.Spotify(auth= token)
+    get_user_playlist(username,sp)
 
-# except:
-#     print('Token is not accesible for ' + username)
+except:
+    print('Token is not accesible for ' + username)
 
 #client_credentials_manager = SpotifyClientCredentials()
 
@@ -39,8 +40,8 @@ def get_playlist_content(username, playlist_id, sp):
             offset += 100
         else:
             break
-    with open('{}-{}'.format(username, playlist_id), 'w') as outfile:
-        json.dump(songs, outfile)
+        with open('{}-{}'.format(username, playlist_id), 'w') as outfile:
+            json.dump(songs, outfile)
 
 def get_playlist_audio_features(username, playlist_id, sp):
     offset = 0
@@ -49,38 +50,38 @@ def get_playlist_audio_features(username, playlist_id, sp):
     ids = []
     
     while True:
-        content = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=100, offset=offset, market=None)
+        content = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=10000, offset=offset, market=None)
         songs += content['items']
         if content['next'] is not None:
             offset += 100
         else:
             break
-    for i in songs:
-        ids.append(i['track']['id'])
-        index = 0
-        audio_features = []
-    while index < len(ids):
-        audio_features += sp.audio_features(ids[index:index + 50])
-        index += 50
+        for i in songs:
+            ids.append(i['track']['id'])
+            index = 0
+            audio_features = []
+            while index < len(ids):
+                audio_features += sp.audio_features(ids[index:index + 50])
+                index += 50
 
-    features_list = []
+        features_list = []
     
-    for features in audio_features:
-        features_list.append([features['energy'], features['liveness'],
-        features['tempo'], features['speechiness'],
-        features['acousticness'], features['instrumentalness'],
-        features['time_signature'], features['danceability'],
-        features['key'], features['duration_ms'],
-        features['loudness'], features['valence'],
-        features['mode'], features['type'],
-        features['uri']])
-        df = pd.DataFrame(features_list, columns=['energy', 'liveness',
-        'tempo', 'speechiness',
-        'acousticness', 'instrumentalness',
-        'time_signature', 'danceability',
-        'key', 'duration_ms', 'loudness',
-        'valence', 'mode', 'type', 'uri'])
-        df.to_csv('{}-{}.csv'.format(username, playlist_id), index=False)
+        for features in audio_features:
+            features_list.append([features['energy'], features['liveness'],
+            features['tempo'], features['speechiness'],
+            features['acousticness'], features['instrumentalness'],
+            features['time_signature'], features['danceability'],
+            features['key'], features['duration_ms'],
+            features['loudness'], features['valence'],
+            features['mode'], features['type'],
+            features['uri']])
+            df = pd.DataFrame(features_list, columns=['energy', 'liveness',
+            'tempo', 'speechiness',
+            'acousticness', 'instrumentalness',
+            'time_signature', 'danceability',
+            'key', 'duration_ms', 'loudness',
+            'valence', 'mode', 'type', 'uri'])
+            df.to_csv('{}-{}.csv'.format(username, playlist_id), index=False)
 
 def get_user_playlist(username, sp):
     playlists = sp.user_playlists(username)
